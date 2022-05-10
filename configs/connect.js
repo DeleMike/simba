beee883c257f81eb9ac8f8cd54d83a8f5aee654c
DeleMike <akindelemichael65@gmail.com>
2022-05-10 13:05:03 +0100

    fix url

--- configs/connect.js
+++ configs/connect.js
@@ -3,14 +3,11 @@ const mongoose = require('mongoose');
 /**
  * Connect to database
  */
-const connectDb = (url) => {
+const connectDb = async (url) => {
    return mongoose.connect(url, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true,
-      }),
-      () => {
-         console.log('connected to db!');
-      }
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   }, ()=> console.log('Connected to db!'));
 }
 
 module.exports = connectDb;
